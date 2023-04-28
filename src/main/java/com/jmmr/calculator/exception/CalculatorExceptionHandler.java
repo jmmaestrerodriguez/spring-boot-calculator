783778dffca31df54e4ab0581a3a498851c05ca9
Jose Manuel Maestre <jmmaestrerodriguez@hotmail.com>
2023-04-28 14:32:09 +0200

    Added exception handling for invalid enum values

--- src/main/java/com/jmmr/calculator/exception/CalculatorExceptionHandler.java
+++ src/main/java/com/jmmr/calculator/exception/CalculatorExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.jmmr.calculator.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,11 +12,15 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 
 @RestControllerAdvice
 public class CalculatorExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final Pattern ENUM_VALUES = Pattern.compile("the values accepted for Enum class:\\s*\\[\\s*((\\S+\\s*,\\s*)*\\S+)\\s*\\]");
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
@@ -41,6 +47,27 @@ public class CalculatorExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(exception, response, headers, response.getStatus(), request);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException exception, HttpHeaders headers, HttpStatus status, WebRequest request){
+        if (exception.getCause() != null && exception.getCause() instanceof InvalidFormatException) {
+            Matcher match = ENUM_VALUES.matcher(exception.getCause().getMessage());
+
+            if (match.find()) {
+                ExceptionResponse response = new ExceptionResponse(
+                        HttpStatus.BAD_REQUEST,
+                        exception.getLocalizedMessage(),
+                        "value should be: " + match.group(1));
+                return handleExceptionInternal(exception, response, new HttpHeaders(), response.getStatus(), request);
+            }
+        }
+        ExceptionResponse response = new ExceptionResponse(
+                HttpStatus.BAD_REQUEST,
+                exception.getLocalizedMessage(),
+                "Bad Request");
+
+        return handleExceptionInternal(exception, response, new HttpHeaders(), response.getStatus(), request);
+    }
+
     @ExceptionHandler({ Exception.class })
     public ResponseEntity<Object> handleAll(Exception exception, WebRequest request) {
         ExceptionResponse response = new ExceptionResponse(
@@ -49,8 +76,5 @@ public class CalculatorExceptionHandler extends ResponseEntityExceptionHandler {
                 "An unknown error has occurred");
         return handleExceptionInternal(exception, response, new HttpHeaders(), response.getStatus(), request);
     }
-
-
-
 
 }
